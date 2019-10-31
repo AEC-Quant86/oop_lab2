67fa8d2898b2c96da35416b4a48e4c23a0dc42c5
Orlov_Ilya <orlov.ilya.istok86@mail.ru>
2019-10-31 23:33:52 +0500

    2.9 done

---
+++ Lab2.9/src/lab2_9.java
@@ -0,0 +1,27 @@
+
+public class lab2_9 {
+
+	public static void main(String[] args) {
+      
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		
+		for (int x : array)
+			System.out.print(x + " ");
+		
+		System.out.println();
+		
+		array = reverse(array);
+		
+		for (int x : array)
+			System.out.print(x + " ");
+	} 
+	
+	  static int[] reverse(int[] arr) {
+	        for (int i = 0; i < arr.length>>1; i++) {
+	            // Меняет два элемента местами:
+	            arr[arr.length-1-i] ^= arr[i] ^= arr[arr.length-1-i];
+	            arr[i] ^= arr[arr.length-1-i];
+	        }
+	        return arr;
+	    }
+}
